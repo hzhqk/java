5694a218342b92ee42756e8e0f4b522d58c1b529
hzhqk <hqk2015@foxmail.com>
2020-07-08 18:22:03 +0800

    Update ExcelField.java
    column order control

--- poi/simple excel/ExcelField.java
+++ poi/simple excel/ExcelField.java
@@ -12,6 +12,21 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface ExcelField {
+    /**
+     * 列名/数据项
+     * @return
+     */
     String name() default "";
+
+    /**
+     * 列顺序，值越小越排在前面
+     * @return
+     */
+    int order() default 0;
+
+    /**
+     * 列格式化器
+     * @return
+     */
     Class<? extends ExcelColumnFormatter> formatter() default NoFormatter.class;
 }
